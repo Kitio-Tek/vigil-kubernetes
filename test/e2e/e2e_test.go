5f007148c25dea4b95e400e64da8300375c37c36
Kitio-Tek <kmyriantek@gmail.com>
2026-05-03 12:55:21 +0200

    tests: update KUTTL specs and e2e suite for athos naming

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -24,10 +24,10 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/Kitio-Tek/vigil-kubernetes/test/utils"
+	"github.com/Kitio-Tek/athos-kubernetes/test/utils"
 )
 
-const namespace = "vigil-system"
+const namespace = "athos-system"
 
 var _ = Describe("controller", Ordered, func() {
 	BeforeAll(func() {
@@ -60,7 +60,7 @@ var _ = Describe("controller", Ordered, func() {
 			var err error
 
 			// projectimage stores the name of the image used in the example
-			var projectimage = "example.com/vigil:v0.0.1"
+			var projectimage = "example.com/athos:v0.0.1"
 
 			By("building the manager(Operator) image")
 			cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectimage))
